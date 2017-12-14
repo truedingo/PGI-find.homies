0a2916c3b72635315f1f60fa47e882e285783098
Diogo Amores <damores@student.dei.uc.pt>
2017-12-14 15:11:12 +0000

    Login and register pages still lead do nothing (need to build a project)

---
+++ app/src/main/java/com/example/dingo/loginscreentryout/signup.java
@@ -0,0 +1,17 @@
+package com.example.dingo.loginscreentryout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Dingo on 12/12/2017.
+ */
+
+public class signup extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+}
