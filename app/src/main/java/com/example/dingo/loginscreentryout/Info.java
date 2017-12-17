c97b2f35c30edb29d6918dfc833b23b20679e72e
Diogo Amores <damores@student.dei.uc.pt>
2017-12-17 04:43:25 +0000

    Info e spinners a funcionar muito bem. Ja com row a ser criadas

--- app/src/main/java/com/example/dingo/loginscreentryout/Info.java
+++ app/src/main/java/com/example/dingo/loginscreentryout/Info.java
@@ -1,8 +1,13 @@
 package com.example.dingo.loginscreentryout;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.*;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -18,8 +23,7 @@ import java.util.List;
 
 public class Info extends AppCompatActivity implements View.OnClickListener{
 
-
-    String [] faculdades={"FDUC","FLUC","FCTUC","FEUC","FFUC","FPCEUC","FCDEFUC","FLUC"};
+    String [] faculdades={"FDUC","FLUC","FCTUC","FEUC","FFUC","FPCEUC","FCDEFUC","FMUC"};
     String [] fctuc={"Geologia","Antropologia","Biologia","Bioquímica","Design e Multimédia","Engenharia Informática","Engenharia e Gestão Industrial","Física","Matemática","Química","Química Medicinal","Arquitetura","Engenharia Cívil","Engenharia do Ambiente","Engenharia Eletrotécnica","Engenharia Mecânica","Engenharia Química","Engenharia Biomédica","Engenharia Física"};
     String [] fmuc={"Medicina","Medicina Dentária"};
     String [] feuc= {"Relações Internacionais","Gestão","Economia"};
@@ -35,7 +39,7 @@ public class Info extends AppCompatActivity implements View.OnClickListener{
         setContentView(R.layout.info);
         List age = new ArrayList<Integer>();
         List faculdade = new ArrayList<String>();
-        List cursos= new ArrayList<String>();
+        final ArrayList<String> cursos= new ArrayList<String>();
         for (String s: faculdades)
             faculdade.add(s);
         for (int i = 16; i <= 65; i++) {
@@ -50,56 +54,60 @@ public class Info extends AppCompatActivity implements View.OnClickListener{
         ArrayAdapter<String> spinnerArrayAdapter1 = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, faculdade);
         spinnerArrayAdapter1.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        Spinner spinner1 = (Spinner)findViewById(R.id.spinner2);
+        final Spinner spinner1 = (Spinner)findViewById(R.id.spinner2);
         spinner1.setAdapter(spinnerArrayAdapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String choice = spinner1.getSelectedItem().toString();
+                cursos.clear();
+                switch (choice) {
+                    case "FDUC":
+                        for (String s : fduc)
+                            cursos.add(s);
+                        break;
+                    case "FLUC":
+                        for (String s : fluc)
+                            cursos.add(s);
+                        break;
+                    case "FEUC":
+                        for (String s : feuc)
+                            cursos.add(s);
+                        break;
+                    case "FCTUC":
+                        for (String s : fctuc)
+                            cursos.add(s);
+                        break;
+                    case "FFUC":
+                        for (String s : ffuc)
+                            cursos.add(s);
+                        break;
+                    case "FPCEUC":
+                        for (String s : fpceuc)
+                            cursos.add(s);
+                        break;
+                    case "FMUC":
+                        for (String s : fmuc)
+                            cursos.add(s);
+                        break;
+                    case "FCDEFUC":
+                        for (String s : fcdefuc)
+                            cursos.add(s);
+                        break;
+                }
+                ArrayAdapter<String> spinnerArrayAdapter2;
+                spinnerArrayAdapter2 = new ArrayAdapter<String>(Info.this, android.R.layout.simple_spinner_item, cursos);
+                spinnerArrayAdapter2.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+                Spinner spinner2 = (Spinner)findViewById(R.id.spinner3);
+                spinner2.setAdapter(spinnerArrayAdapter2);
+            }
 
-        String choice = spinner1.getSelectedItem().toString();
-        switch (choice) {
-            case "FDUC":
-                for (String s : fduc)
-                    cursos.add(s);
-                break;
-            case "FLUC":
-                for (String s : fluc)
-                    cursos.add(s);
-                break;
-            case "FEUC":
-                for (String s : feuc)
-                    cursos.add(s);
-                break;
-            case "FCTUC":
-                for (String s : fctuc)
-                    cursos.add(s);
-                break;
-            case "FFUC":
-                for (String s : ffuc)
-                    cursos.add(s);
-                break;
-            case "FPCEUC":
-                for (String s : fpceuc)
-                    cursos.add(s);
-                break;
-            case "FMUC":
-                for (String s : fmuc)
-                    cursos.add(s);
-                break;
-            case "FCDEFUC":
-                for (String s : fcdefuc)
-                    cursos.add(s);
-                break;
-        }
-        ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(
-                this, android.R.layout.simple_spinner_item, cursos);
-        spinnerArrayAdapter1.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
-        Spinner spinner2 = (Spinner)findViewById(R.id.spinner3);
-        spinner2.setAdapter(spinnerArrayAdapter2);
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
 
-
-
-
-
-
-
+        });
 
 
     }
