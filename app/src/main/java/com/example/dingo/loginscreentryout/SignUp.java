5bee3157bf8749f2fc68ab82b31fb2bae6c47f97
Diogo Amores <damores@student.dei.uc.pt>
2017-12-18 00:55:30 +0000

    Parte 1 of create account implementada

--- app/src/main/java/com/example/dingo/loginscreentryout/SignUp.java
+++ app/src/main/java/com/example/dingo/loginscreentryout/SignUp.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,20 +15,60 @@ import com.google.firebase.auth.FirebaseAuth;
  * Created by Dingo on 16/12/2017.
  */
 
-public class SignUp extends AppCompatActivity implements View.OnClickListener{
+public class SignUp extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private String savedEmail;
+    private String savedPassword;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        Button bContinue = findViewById(R.id.bContinue);
+        final EditText sEmail = findViewById(R.id.editText3);
+        final EditText sPassword = findViewById(R.id.editText2);
+        final EditText cPassword = findViewById(R.id.editText5);
+
+        bContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savedEmail = sEmail.getText().toString();
+                savedPassword = sPassword.getText().toString();
+                String confPassword = cPassword.getText().toString();
+                if(!savedEmail.equals("") && !savedPassword.equals("") && !confPassword.equals("")){
+                    if(savedPassword.equals(confPassword)){
+                        startActivity(new Intent(SignUp.this, Info.class));
+                    }
+                    else{
+                        toastMessage("As passwords precisam de ser iguais!");
+                    }
+                }
+                else{
+                    toastMessage("É necessário preencher todos os campos!");
+                }
+            }
+        });
+
     }
-    @Override
-    public void onClick(View view) {
-        int i = view.getId();
-        if(i == R.id.bContinue) {
-            startActivity(new Intent(SignUp.this, Info.class));
-        }
+
+    public String getSavedEmail(){
+        return this.savedPassword;
     }
+
+    public String getSavedPassword(){
+        return this.savedPassword;
+    }
+
+    private void toastMessage(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onStart(){
+        super.onStart();
+    }
+
 }
